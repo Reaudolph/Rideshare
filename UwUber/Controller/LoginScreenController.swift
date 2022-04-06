9b30079aadb0aa8f5deb0eb25ee89b88a2a9d7d8
Hrishikesh <hrishivik@Gmail.com>
2022-04-06 13:47:44 -0500

    Login Finished

--- UwUber/Controller/LoginScreenController.swift
+++ UwUber/Controller/LoginScreenController.swift
@@ -6,9 +6,28 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginScreenController: UIViewController {
-
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBAction func LoginButtonPressed(_ sender: Any) {
+        guard let userPassword = passwordTextField.text else {return }
+        guard let email  = emailTextField.text else {return}
+        
+        Auth.auth().signIn(withEmail: email, password: userPassword) { [weak self] authResult, error in
+            print("coundnt \(error?.localizedDescription)")
+            guard let strongSelf = self else { return }
+          // ...
+        }
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 //        title = "Login"
